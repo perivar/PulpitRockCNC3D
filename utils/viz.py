063247cafd5510a008b6d5b89bbadf5ed8219261
Per Ivar Nerseth <perivar@nerseth.com>
2015-11-17 21:52:32 +0100

    Improved the blender rendering of STL files

--- utils/viz.py
+++ utils/viz.py
@@ -3,10 +3,16 @@ import sys
 
 global selObj
 global camTarget
-mat = 'abs' # The material to use on the imported STL is called "abs"  
-            # e.g. orange plastic with white gloss 
-            # this is saved with as "F abs" so that it is saved with the blend file even
-            # if the material isn't used
+
+# Material
+# The material to use on the imported STL is called "abs"  
+# e.g. orange plastic with white gloss 
+# this is saved with as "F abs" so that it is saved with the blend file even
+# if the material isn't used
+#mat = 'abs'
+#mat = 'stainless_steel'
+mat = 'chrome'
+#mat = 'plastic'
 
 # load stl file			
 def load_stl(file_path):
@@ -15,13 +21,15 @@ def load_stl(file_path):
     # load stl
     bpy.ops.import_mesh.stl(filepath=file_path)
     
-    # select properly
+    # select properly by deselection all and selecting again
     selObj = bpy.context.selected_objects[0]
     print('Selected object: ', selObj)
+    # deselect all
     bpy.ops.object.select_all(action='DESELECT')
+    # and select only the new object 
     selObj.select = True
     	
-    # set origin
+    # set origin so that the later transform (move) will be correct
     bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN', center='BOUNDS')
     
     # place
@@ -32,9 +40,9 @@ def load_stl(file_path):
     zDim = selObj.dimensions[2]
     print('Z dimension: ', zDim)
 	
-    # Translate (move) selected items half it's height upwards
+    # Translate (move) selected items half it's own height upwards
     bpy.ops.transform.translate(value=(0,0,zDim/2.0))
-    # Note this throws an error 'convertviewvec called in invalid context'
+    # Note this throws an error 'convertViewVec: called in an invalid context'
 
     # calculate camera target in the z axis
     camTarget = (0,0,zDim/3.0)
@@ -52,17 +60,17 @@ def place_camera():
     y = d[1]
     z = d[2]
 	
-    # scale z and y down with 25%
+    # scale x and y down with 25%
     max_dim = max(x * 0.75, y * 0.75, z)
     print('Max dimensions: ', max_dim)
 	
-    # place camera at the target calculated in the load_stl method
+    # point camera at the target calculated in the load_stl method
     # this require a special 'empty - target - sphere' element named 'target'
     bpy.data.objects['target'].location = camTarget
 
     # and the camera named 'Camera'
-    cam = bpy.data.objects['Camera'].location.x = max_dim * 2.4
-
+    cam = bpy.data.objects['Camera'].location.x = max_dim * 4.0
+    
 def render_image(image,gl=False,anim=False):
     # render image
     if gl:
