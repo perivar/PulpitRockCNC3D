d567762a354e1aaabcbae2fbf6045f204778645c
Per Ivar Nerseth <perivar@nerseth.com>
2015-11-28 15:32:57 +0100

    Added renders

--- RENDER_STL_NEW.bat
+++ RENDER_STL_NEW.bat
@@ -1,6 +1,6 @@
-blender -b  utils/Plane_mesh_studio_model.blend -P utils/viz2.py -- MotorAndCoupling.stl MotorAndCoupling.png
+rem blender -b  utils/Plane_mesh_studio_model.blend -P utils/viz2.py -- MotorAndCoupling.stl MotorAndCoupling.png
 
-REM blender -b  utils/Plane_mesh_studio_model.blend -P utils/viz2.py -- "20-GT2-6 Timing Pulley.stl" "20-GT2-6 Timing Pulley.png"
+rem blender -b  utils/Plane_mesh_studio_model.blend -P utils/viz2.py -- "20-GT2-6 Timing Pulley.stl" "20-GT2-6 Timing Pulley.png"
 
-REM blender -b  utils/Plane_mesh_studio_model.blend -P utils/viz2.py -- "flexible_coupling.stl" "flexible_coupling.png"
+blender -b  utils/Plane_mesh_studio_model.blend -P utils/viz2.py -- "flexible_coupling.stl" "flexible_coupling.png"
 
